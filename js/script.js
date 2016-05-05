521e6fd9377eaca8ca968a08d4eb7e1ae8ddb6e2
bibixx <bartosz@legiec.eu>
2016-05-05 16:53:19 +0200

    preety much finished settings

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-$(".disabled").hide().removeClass("disabled")
-
 var teams = [
   {
     "player": "Bartek",
@@ -74,7 +72,6 @@ results.sort(function(a,b){
         return 1;
     }
 });
-
 results.reverse();
 
 for(x=0; x<results.length; x++){
@@ -95,16 +92,23 @@ for(x=0; x<results.length; x++){
   $("tbody").append( tr )
 }
 
+// TABLE END
+
+$(".disabled").hide().removeClass("disabled")
 
 $(".spinner input").bind("input", function(){
   var str = $(this).val();
   var res = str.replace(/[^0-9]/g, "");
-  $(this).val( res )
+  if(res == ""){
+    res = "0"
+  }
+  $(this).val( res*1 )
 })
 
 $(".spinner .plus").bind("click", function(){
   var $p = $(this).siblings("input");
   $p.val( $p.val()*1+1 );
+  $p.trigger("input")
   $(this).blur();
 })
 
@@ -113,14 +117,17 @@ $(".spinner .minus").bind("click", function(){
   if( $p.val() > 1 ){
     $p.val( $p.val()*1-1 );
   }
+  $p.trigger("input")
   $(this).blur();
 })
 
 $("#teams").bind("change", function(){
   if( $(this).prop("checked") ){
     $("#teamsNo").parents(".row").stop().slideDown(200)
+    $("#teamsNo").val( Math.round( $("#players").val()/2 ) ).trigger("input")
   } else {
     $("#teamsNo").parents(".row").stop().slideUp(200)
+    $("#teamsNo").trigger("input")
   }
 })
 
@@ -132,3 +139,94 @@ $("#type").bind("change", function(){
     $("#matchesvs").parents(".row").stop().slideUp(200)
   }
 })
+
+$("#players").bind("input", function(){
+  if( $("#players").val() < 2 ){
+    $("#players").val( 2 )
+  }
+  if( $("#teams").prop("checked") ){
+    if( $("#players").val()/$("#teamsNo").val() < 1 ){
+      $("#teamsNo").val( $("#players").val() )
+    }
+    if( $("#players").val()/$("#teamsNo").val() > 2 ){
+      $("#teamsNo").val( Math.round( $("#players").val()/2 ) )
+    }
+  }
+})
+
+$("#teamsNo").bind("input", function(){
+  if( $("#teamsNo").val() < 2 ){
+    $("#teamsNo").val( 2 )
+  }
+  if( $("#teams").prop("checked") ){
+    if( $("#players").val() > $("#teamsNo").val()*2 ){
+      $("#players").val( $("#teamsNo").val()*2 )
+    }
+    if( $("#players").val() / $("#teamsNo").val() < 1 ){
+      $("#players").val( $("#teamsNo").val() )
+    }
+  }
+})
+
+$("#players, #teamsNo").bind("input", function(){
+  printTeams();
+})
+
+$("#preclubs").bind("change", function(){
+  printTeams();
+})
+
+printTeams();
+
+function printTeams(){
+  $(".teams .team").remove();
+  var playersNo = $("#players").val()*1+1
+  var teamsNo = $("#teamsNo").val()*1+1;
+  var players = distribute();
+  if( $("#teams").prop("checked") ){
+    y = teamsNo
+  } else {
+    y = playersNo
+  }
+  for(x=1; x<y; x++){
+    var $team = $("<div></div>").attr("id", x).addClass("team")
+    var temp = 1
+    if( !$("#teams").prop("checked") ){
+      $team.append( $("<input>").attr({"type": "text", 'name': "name1", "placeholder": "Player"}) )
+    } else {
+      for(i=0; i<players[x-1]; i++){
+        $team.append( $("<input>").attr({"type": "text", 'name': "name"+(i+1), "placeholder": "Player "+(i+1)}) )
+      }
+    }
+    if( $("#preclubs").prop("checked") ){
+      $team.append( $("<input>").attr({"type": "text", 'name': "club", "placeholder": "Club name"}) )
+    }
+    $(".teams").append( $team )
+  }
+}
+
+function distribute(){
+  var items = [];
+  var columns = [];
+  var lengths = [];
+  var its = $("#players").val()*1;
+  var cols = $("#teamsNo").val()*1;
+  for(x=0; x<its; x++){
+  	items.push([]);
+  }
+  for(x=0; x<cols; x++){
+  	columns.push([]);
+  }
+
+  for (x=0; x<items.length; x++) {
+    columns[Math.floor(x * columns.length / items.length)].push(items[x]);
+  }
+
+  for(x=0; x<columns.length; x++){
+  	lengths[x] = columns[x].length;
+  }
+
+  lengths.sort(function(a,b){return b-a});
+
+  return lengths;
+}
